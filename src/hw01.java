3f174a9aae893957a42a0c44f6b5e629dfad237d
Ilya Cheshkov <0921408@gmail.com>
2022-02-26 00:02:06 +0300

    1

---
+++ src/hw01.java
@@ -0,0 +1,5 @@
+public class hw01 {
+    public static void main(String[] args) {
+        System.out.println("3\n2\n1\nПоехали!!!");
+    }
+}
---
+++ src/hw02.java
@@ -0,0 +1,15 @@
+public class hw02 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 8;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.println("Число a " + (a % 2 == 0 ? "четное" : "нечетное"));
+        System.out.println("Число b " + (b % 2 == 0 ? "четное" : "нечетное"));
+    }
+}
