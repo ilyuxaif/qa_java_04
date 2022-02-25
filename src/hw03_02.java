ded787a59159a073677b11b21bd92c806eca1ae4
Ilya Cheshkov <0921408@gmail.com>
2022-02-26 01:48:32 +0300

    hw03

---
+++ src/hw03_02.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class hw03_02 {
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * (1000 - 0 + 1) + 0);
+        int b = (int) (Math.random() * (1000 - 0 + 1) + 0);
+        System.out.printf("a = %d, b = %d\n", a, b);
+        System.out.println(((a + b) % 2 == 0) ? "maybe a and b are even" : "some variable is odd");
+
+    }
+}
