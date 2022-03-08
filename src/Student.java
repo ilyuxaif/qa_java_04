31a012325f1985dcf16e4e6698e3120d1b68cbeb
Ilya Cheshkov <0921408@gmail.com>
2022-03-09 01:18:56 +0300

    hw06_extra

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    public static void main(String[] args) {
+        String line = "----------------------------------";
+
+        String name = "Вова Сидоров";
+        String subject = "ученик 5го класса";
+        int age = 10;
+
+        System.out.println(line);
+        System.out.println("Карточка ученика:");
+        System.out.println("Имя: " + name);
+        System.out.println("Должность: " + subject);
+        System.out.println("Возраст: " + age);
+        System.out.println(line);
+
+        String name1 = "Маша Семенова";
+        String subject1 = "ученица 9-го класса";
+        int age1 = 14;
+
+        System.out.println("Карточка ученицы:");
+        System.out.println("Имя: " + name1);
+        System.out.println("Должность: " + subject1);
+        System.out.println("Возраст: " + age1);
+        System.out.println(line);
+    }
+
+}
---
+++ src/Teacher.java
@@ -0,0 +1,27 @@
+public class Teacher {
+    public static void main(String[] args) {
+        String line = "----------------------------------";
+
+        String name = "Мариванна";
+        String subject = "Учитель русского языка";
+        int age = 25;
+
+        System.out.println(line);
+        System.out.println("Карточка учителя:");
+        System.out.println("Имя: " + name);
+        System.out.println("Должность: " + subject);
+        System.out.println("Возраст: " + age);
+        System.out.println(line);
+
+        String name1 = "Петрович";
+        String subject1 = "Учитель труда";
+        int age1 = 49;
+
+        System.out.println("Карточка учителя:");
+        System.out.println("Имя: " + name1);
+        System.out.println("Должность: " + subject1);
+        System.out.println("Возраст: " + age1);
+        System.out.println(line);
+    }
+
+}
