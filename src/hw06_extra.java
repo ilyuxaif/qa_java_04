8df3edfe543a7cf2a543893d2bdfc990fe645c4a
Ilya Cheshkov <0921408@gmail.com>
2022-03-26 22:14:28 +0300

    hw06_extra_

--- src/hw06_extra.java
+++ src/hw06_extra.java
@@ -6,20 +6,27 @@
         необходимо подсчитать количество строк в массиве, которые не содержат буквы “е”.
 */
 
+import java.util.Arrays;
+
 public class hw06_extra {
     public static void main(String[] args) {
         String[][] array = {{"Привет", "всем", "кто"},
                             {"изучает", "язык", "программирования"},
                             {"java"}};
         int counter = 0;
+        String[] arrayOutOfE = new String[10];
+        int index = 0;
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (!(array[i][j].contains("е"))) {
                     counter ++;
+                    arrayOutOfE[index] = array[i][j];
+                    index++;
                 }
             }
         }
         System.out.println(counter);
+        System.out.println(Arrays.toString(arrayOutOfE));
     }
 }
