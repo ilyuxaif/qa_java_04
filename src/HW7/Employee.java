eec914fe5b5096b9b8597485f6e6e0933d4aa718
Ilya Cheshkov <0921408@gmail.com>
2022-04-15 19:55:59 +0300

    hw07

--- src/HW7/Employee.java
+++ src/HW7/Employee.java
@@ -1,7 +1,38 @@
 package HW7;
 
+/**
+ * Необходимо создать класс Employee со следующими методами:
+ * getBaseSalary - получить базовую ставку
+ * set Base Salary
+ * getName - получить имя
+ * setName
+ * getSalary - получить зарплату
+ */
+
 public class Employee {
 
+    protected int baseSalary;
+    private int salary;
+    private String name;
 
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    int getSalary() {
+        return salary;
+    }
 
 }
